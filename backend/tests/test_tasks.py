3dfdff7da514f441eee397977a2fcc7dc00d5216
Alessa Messineo <apmessineo@gmail.com>
2024-03-06 17:56:20 -0500

    add tearDown, PUT and DEL tests

--- backend/tests/test_tasks.py
+++ backend/tests/test_tasks.py
@@ -13,6 +13,11 @@ class TestTasksBlueprint(unittest.TestCase):
         with self.app.app_context():
             db.create_all()
 
+    def tearDown(self):
+        with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
+
     def test_get_tasks(self):
         response = self.client.get('/tasks')
         data = response.get_json()
@@ -27,3 +32,26 @@ class TestTasksBlueprint(unittest.TestCase):
 
         self.assertEqual(response.status_code, 201)
         self.assertEqual(data['message'], 'Task successfully created')
+
+    def test_update_task(self):
+        task = Task(title='Task', description='Description', completed=False)
+        db.session.add(task)
+        db.session.commit()
+
+        updated_data = {'title': 'Updated Task', 'description': 'Updated Description', 'completed': True}
+        response = self.client.put('/tasks/{}'.format(task.id), json=updated_data)
+        data = response.get_json()
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(data['message'], 'Task successfully updated')
+
+    def test_delete_task(self):
+        task = Task(title='Test Task', description='Test Description', completed=False)
+        db.session.add(task)
+        db.session.commit()
+
+        response = self.client.delete('/tasks/{}'.format(task.id))
+        data = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['message'], 'Task successfully deleted')
