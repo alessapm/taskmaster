dea6240d9ff2b72fcfaf544d089c139a9df3054f
Alessa Messineo <apmessineo@gmail.com>
2024-03-06 17:46:25 -0500

    add GET and POST tests

--- backend/tests/test_tasks.py
+++ backend/tests/test_tasks.py
@@ -14,9 +14,16 @@ class TestTasksBlueprint(unittest.TestCase):
             db.create_all()
 
     def test_get_tasks(self):
-        # Test the GET /tasks endpoint
         response = self.client.get('/tasks')
         data = response.get_json()
 
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(data, list)
+
+    def test_create_task(self):
+        task_data = {'title': 'Test Task', 'description': 'Test Description', 'completed': False}
+        response = self.client.post('/tasks', json=task_data)
+        data = response.get_json()
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(data['message'], 'Task successfully created')
