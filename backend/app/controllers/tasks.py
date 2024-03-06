d2c5bc4378f671680ec36dea3e22e9514abfeca7
Alessa Messineo <apmessineo@gmail.com>
2024-03-06 15:41:05 -0500

    add POST and PUT methods

--- backend/app/controllers/tasks.py
+++ backend/app/controllers/tasks.py
@@ -11,10 +11,35 @@ def root():
 
 @tasks_blueprint.route('/tasks', methods=['GET', 'POST'])
 def handle_tasks():
-    print('hello')
     if request.method == 'GET':
         tasks = Task.query.all()
         return jsonify([{'id': task.id, 'title': task.title, 'description': task.description, 'completed': task.completed} for task in tasks]), 200
-    # Add POST logic here
+   
+   elif request.method == 'POST':
+       data = request.get_json
 
-# Add more routes here if needed
+       new_task = Task(
+           title = data.get('title'),
+           description = data.get('description')
+           completed = data.get('completed', False)
+       )
+
+       db.session.add(new_task)
+       db.session.commit()
+
+       return jsonify({'message': 'Task successfully created'}), 201
+
+def edit_task(id):
+    task = Task.query.get_or_404(id)
+
+    if request.method == 'PUT':
+        data = request.get.json()
+
+        task.title = data.get('title', task.title),
+        task.description = data.get('description', task.description)
+        task.completed = data.get('completed', task.completed)
+
+        db.session.commit()
+
+        return jsonify({'message': 'Task successfully updated'}), 201
+
