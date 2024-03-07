1fb966013c8605415fc0ed74b62df5474a7834d2
Alessa Messineo <apmessineo@gmail.com>
2024-03-07 13:37:30 -0500

    add getTasks req. Add task li.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { useState, useEffect } from 'react';
 import { getTaskData } from "./taskService";
 
 
 export default function App() {
-
+  const [ tasks, setTasks ] = useState([]);
+  
+  useEffect(() => {
+    const getTasks = async () => {
+      try {
+        const tasksData = await getTaskData();
+        setTasks(tasksData);
+      } catch (err) {
+        console.log('error fetching data: ', err);
+      }
+    }
+    getTasks();
+    console.log('tasks: ', tasks)
+  }, []);
 
   return (
     <div className="App">
@@ -13,6 +27,16 @@ export default function App() {
           Task Master
         </h1>
       </header>
+      <div className="main">
+        Tasks: 
+        {!tasks ? <p>No Current Tasks</p> 
+        : tasks.map((task) => (
+          <li key={task.id}>
+            {task.title}
+          </li>
+        ))
+        }
+      </div>
     </div>
   );
 }
